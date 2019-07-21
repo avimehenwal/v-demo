c88ca1c5dc99bab43a3e27cf29bb8d10076efaf7
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 23:53:30 +0200

    text-size with display
    https://vuetifyjs.com/en/framework/typography#typography

--- v-task/src/views/Home.vue
+++ v-task/src/views/Home.vue
@@ -9,6 +9,15 @@
     <p class="lighten-4 pink red--text text--darken-4">
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, vero expedita modi, quod provident iusto unde hic numquam ipsam, sapiente quidem cupiditate nesciunt minima consequuntur iste. Nisi enim blanditiis dignissimos?
     </p>
+    <h4 class="display-1">
+      Smaller text
+    </h4>
+    <h1>
+      Normal h1 heading
+    </h1>
+    <p class="headline">headline</p>
+    <p class="subheading">subheading</p>
+    <p class="caption">caption to place under image</p>
 
   </div>
 </template>
