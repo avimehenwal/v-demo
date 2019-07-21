c0be5b95718b6b00c0e539d60e5c6766d3600135
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 22:45:17 +0200

    Seperate header component

---
+++ v-shoppinglist/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title class="headline text-uppercase">
+    <span>My awesome</span>
+    <span class="font-weight-light">shoppinglist app</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn
+    flat
+    href="https://github.com/vuetifyjs/vuetify/releases/latest"
+    target="_blank"
+    >
+    <span class="mr-2">Latest Release</span>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
