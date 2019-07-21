9bd0b4a6acae75660487a73dae6c9451571a2699
avimehenwal <avi.mehanwal@gmail.com>
2019-07-22 00:54:22 +0200

    Navigation drawer
    doesnt work without app prop in v-navigation-drawer

---
+++ v-task/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+<nav>
+    <v-toolbar flat app>
+        <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title class="grey--text text-uppercase">
+            <span class="font-weight-light">ToDo</span>
+            <span>Ninja</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn flat color="grey">
+            <span>Sign Out</span>
+            <v-icon right>exit_to_app</v-icon>
+        </v-btn>
+    </v-toolbar>
+
+    <!-- Navigation drawer -->
+    <v-navigation-drawer app class="indigo" v-model="drawer">
+        <p>test</p>
+    </v-navigation-drawer>
+</nav>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            drawer: false,
+        })
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
