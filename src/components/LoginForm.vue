ef72525c14fd35081f2af292e8924aaa7453cfc3
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 17:56:01 +0200

    Add v-btn-action subcomponents for submit/cancel v-btn

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,7 +4,6 @@
       <v-card-title>
         <h1>Login</h1>
       </v-card-title>
-
       <v-card-text>
         <v-form>
           <v-text-field label="Username" />
@@ -14,7 +13,10 @@
           />
         </v-form>
       </v-card-text>
-
+       <v-card-actions>
+        <v-btn>Register</v-btn>
+        <v-btn>Login</v-btn>
+      </v-card-actions>
     </v-card>
   </div>
 </template>
