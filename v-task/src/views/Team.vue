e6d3dfb23b9530e80e2124513cc9bcb911de6253
avimehenwal <avi.mehanwal@gmail.com>
2019-07-22 01:55:07 +0200

    Components as per routes with dummy content

---
+++ v-task/src/views/Team.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="team">
+    <h1 class="subheading grey--text">
+      team
+    </h1>
+
+    <v-container class="my-5">
+        content
+    </v-container>
+</div>
+</template>
+
+<script>
+
+  export default {
+    components: {
+    }
+  }
+</script>
