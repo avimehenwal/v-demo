4695d4dd51de2cadf4aac9c583e4be373ec4ca0e
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 17:30:05 +0200

    Add a new component

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-toolbar dark color="primary">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text">v-demo title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: 'TitleBar',
+        props:  {
+            // Title
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
