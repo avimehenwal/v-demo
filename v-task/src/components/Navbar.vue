f2079adcf1e2610bdb82b41b6f8d6b346ed26684
avimehenwal <avi.mehanwal@gmail.com>
2019-07-22 01:22:31 +0200

    link drawer items from js data

--- v-task/src/components/Navbar.vue
+++ v-task/src/components/Navbar.vue
@@ -16,13 +16,13 @@
     <!-- Navigation drawer -->
     <v-navigation-drawer app class="primary" v-model="drawer">
         <v-list>
-            <v-list-tile>
+            <v-list-tile v-for="link in links" :key="link.text">
                 <v-list-tile-action>
-                    <v-icon dark>dashboard</v-icon>
+                    <v-icon dark>{{ link.icon }}</v-icon>
                 </v-list-tile-action>
                 <v-list-tile-content>
                     <v-list-tile-title class="white--text">
-                        Dashboard
+                        {{ link.text }}
                     </v-list-tile-title>
                 </v-list-tile-content>
             </v-list-tile>
