b1a6fd5b12b75e3e0aa1327c67a0837b61c91a57
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 23:40:49 +0200

    ROboto font by default with vuetify

--- v-task/src/views/Home.vue
+++ v-task/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    Welcome to home
+  <div class="home">
+    <h1>
+      Welcome to home
+    </h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quod possimus placeat, tempore repellendus porro repudiandae unde iusto atque dicta, error dolorem magnam facilis ipsa iste omnis eum saepe vitae?
+    </p>
   </div>
 </template>
 
