60634726a1d0b3e5adc3e811999b4957d51abfee
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 22:13:39 +0200

    Make header a component

--- v-shoppinglist/src/App.vue
+++ v-shoppinglist/src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>My awesome</span>
-        <span class="font-weight-light">shoppinglist app</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
+    <Header/>
     <HelloWorld/>
     <Footer/>
   </v-app>
@@ -23,12 +9,14 @@
 <script>
 import HelloWorld from './components/HelloWorld'
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     Footer,
+    Header
   },
   data () {
     return {
