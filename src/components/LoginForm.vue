4a45387be41679191061567ac4544087df5f1618
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 18:12:22 +0200

    Adjust width and details like spaces and hr

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-card>
+    <v-card width="50%" class="mt-5 mx-a">
       <v-card-title>
         <h1>Login</h1>
       </v-card-title>
@@ -18,8 +18,10 @@
           />
         </v-form>
       </v-card-text>
-       <v-card-actions>
+      <v-divider></v-divider>
+      <v-card-actions>
         <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
         <v-btn color="info">Login</v-btn>
       </v-card-actions>
     </v-card>
