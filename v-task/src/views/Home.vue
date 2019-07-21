936ea36a43cb101918e0e5cf4e9d3f893754c7f5
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 23:48:07 +0200

    lighten and darken text color and bg
    https://vuetifyjs.com/en/framework/colors#colors

--- v-task/src/views/Home.vue
+++ v-task/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home">
-    <h1>
-      Welcome to home
+    <h1 class="display-4">
+      Home
     </h1>
     <p class="blue white--text">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quod possimus placeat, tempore repellendus porro repudiandae unde iusto atque dicta, error dolorem magnam facilis ipsa iste omnis eum saepe vitae?
     </p>
+    <p class="lighten-4 pink red--text text--darken-4">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, vero expedita modi, quod provident iusto unde hic numquam ipsam, sapiente quidem cupiditate nesciunt minima consequuntur iste. Nisi enim blanditiis dignissimos?
+    </p>
+
   </div>
 </template>
 
