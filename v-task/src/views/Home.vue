7e8ec20d18000db925703f59cfdbd9a1f22695a9
avimehenwal <avi.mehanwal@gmail.com>
2019-07-21 23:43:22 +0200

    text and element background color

--- v-task/src/views/Home.vue
+++ v-task/src/views/Home.vue
@@ -3,7 +3,7 @@
     <h1>
       Welcome to home
     </h1>
-    <p>
+    <p class="blue white--text">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quod possimus placeat, tempore repellendus porro repudiandae unde iusto atque dicta, error dolorem magnam facilis ipsa iste omnis eum saepe vitae?
     </p>
   </div>
