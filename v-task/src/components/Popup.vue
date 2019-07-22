6839a3c56036fcc3c8eb076d22d86dae0f52a4fd
avimehenwal <avi.mehanwal@gmail.com>
2019-07-22 19:14:17 +0200

    Add dialog window

---
+++ v-task/src/components/Popup.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-dialog max-width="600px">
+        <v-btn flat slot="activator" class="success">
+            Add new project
+        </v-btn>
+        <v-card>
+            <v-card-title>
+                <h2>Add a new project</h2>
+            </v-card-title>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+
+    })
+</script>
+
+<style scoped>
+
+</style>
