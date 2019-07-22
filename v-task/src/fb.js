5da1c4b9ede81cb96cd444cdafb36212d024cc75
avimehenwal <avi.mehanwal@gmail.com>
2019-07-22 21:38:46 +0200

    Connect to firebase db

---
+++ v-task/src/fb.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBWKKDzkWQ5fLZAcPxX9YL9BET8hxqiYco",
+    authDomain: "web-avimehenwal-in.firebaseapp.com",
+    databaseURL: "https://web-avimehenwal-in.firebaseio.com",
+    projectId: "web-avimehenwal-in",
+    storageBucket: "web-avimehenwal-in.appspot.com",
+    messagingSenderId: "7741353385",
+    appId: "1:7741353385:web:622674ecdd2c3f07"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+// avoid warning in console
+db.settings({ timestampsInSnapshots: true });
+
+//export to make it available to other files
+export default db;
