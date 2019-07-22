73c94e42b1e061c7f15a363f449fb59e19176028
avimehenwal <avi.mehanwal@gmail.com>
2019-07-22 19:26:58 +0200

    Add form in dialog

--- v-task/src/components/Popup.vue
+++ v-task/src/components/Popup.vue
@@ -7,16 +7,34 @@
             <v-card-title>
                 <h2>Add a new project</h2>
             </v-card-title>
+            <v-card-text>
+                <v-form class="px-3">
+                    <v-text-field label="Title" prepend-icon="folder" v-model="title">
+                    </v-text-field>
+                    <v-textarea label="Information" prepend-icon="edit" v-model="content"></v-textarea>
+                    <v-btn flat class="success mx-0 mt-3" @click="submit">+ Add Project</v-btn>
+                </v-form>
+            </v-card-text>
         </v-card>
     </v-dialog>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
 
-    export default Vue.extend({
+export default {
+    data() {
+        return {
+            title: '',
+            content: '',
+        }
+    },
+    methods: {
+        submit() {
+            console.log(this.title, this.content)
+        }
+    }
 
-    })
+}
 </script>
 
 <style scoped>
