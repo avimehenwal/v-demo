7b3c703d88cc1356712ebfd7aaf834e542fe1470
avimehenwal <avi.mehanwal@gmail.com>
2020-03-04 19:03:59 +0100

    Random data to rows

--- v-chart/src/App.vue
+++ v-chart/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    {{chart_data}} {{randomNumber()}}
+    {{chartData}} {{randomNumber()}}
     <GChart
       type="ColumnChart"
       :data="chartData"
@@ -10,33 +10,44 @@
 </template>
 
 <script>
-
-const chartData = [
-    ['Year', 'Sales', 'Expenses', 'Profit'],
-    ['2014', 1000, 400, 200],
-    ['2015', 1170, 460, 250],
-    ['2016', 660, 1120, 300],
-    ['2017', 1030, 540, 350]
-];
+// const chartData = [
+//     ['Year', 'Sales', 'Expenses', 'Profit'],
+//     ['2014', 1000, 400, 200],
+//     ['2015', 1170, 460, 250],
+//     ['2016', 660, 1120, 300],
+//     ['2017', 1030, 540, 350]
+// ];
 
 export default {
   name: 'App',
+  created() {
+    this.addRow();
+  },
   data: () => ({
-    chartData: chartData,
+    chartData: [
+        ['Year', 'Sales', 'Expenses', 'Profit']
+    ],
     chartOptions: {
       chart: {
         title: 'Company Performance',
         subtitle: 'Sales, Expenses, and Profit: 2014-2017',
       }
     },
-    random_max: 1000,
-    random_min: 200
+    start_year: 2015
   }),
   methods : {
     randomNumber : function() {
       var random_max = 1000;
       var random_min = 200;
       return Math.floor(Math.random() * random_max) + random_min;
+    },
+    addRow : function() {
+      var i;
+      for (i = this.start_year; i < this.start_year + 5; i++) {
+        let row = [i, this.randomNumber(), this.randomNumber(), this.randomNumber()];
+        console.log(row);
+        this.chartData.push(row);
+      }
     }
   }
 };
