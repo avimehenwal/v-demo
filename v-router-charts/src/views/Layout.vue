825befb1c9e344635aaf1256b18a9b693f2f6853
avimehenwal <avi.mehanwal@gmail.com>
2020-03-10 13:25:43 +0100

    Computed vs Methods in vuejs

--- v-router-charts/src/views/Layout.vue
+++ v-router-charts/src/views/Layout.vue
@@ -1,10 +1,11 @@
 <template>
   <v-container class="grey lighten-5">
     <v-row no-gutters fluid>
-      <v-col v-for="n in 3" :key="n" cols="12" sm="4" >
+      <!-- All grid space in extra small devices, half on small and quarter on medium -->
+      <v-col v-for="n in 3" :key="n" cols="12" xs="12" sm="6" md="4">
         <v-card flat>
           <v-card-text class="display-4 text-center">221</v-card-text>
-          <v-card-subtitle class="text-center">Processes</v-card-subtitle>
+          <v-card-subtitle class="text-center">{{}}</v-card-subtitle>
           <!-- <v-card-text>Processes</v-card-text> -->
         </v-card>
       </v-col>
@@ -25,6 +26,18 @@
         </v-card>
       </v-col>
     </v-row>
+
+  <v-card>
+    <v-card-text>
+      Methods : {{ dateM }} <br>
+      Computed : {{ getComputed }}
+    </v-card-text>
+    <v-card-actions>
+      <v-btn v-on:click="getDate()">Refresh methods</v-btn>
+      <v-btn v-on:click="getComputed()">Refresh computed</v-btn>
+    </v-card-actions>
+  </v-card>
+
   </v-container>
 </template>
 
@@ -34,7 +47,18 @@ export default {
     fill: true,
     radius: 10,
     value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
-    width: 2
-  })
+    width: 2,
+    dateM: ''
+  }),
+  methods: {
+    getDate: function () {
+      this.dateM = Date.now()
+    }
+  },
+  computed: {
+    getComputed: function () {
+      return Date.now()
+    }
+  }
 }
 </script>
