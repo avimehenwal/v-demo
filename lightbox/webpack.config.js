6fec81b65392dc3b705656d733e4dd9b7a242908
avimehenwal <avi.mehanwal@gmail.com>
2020-08-19 15:52:43 +0200

    feat(webpack): :poop: Use config from package.json inside webpack.config

--- lightbox/webpack.config.js
+++ lightbox/webpack.config.js
@@ -1,12 +1,15 @@
+const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
-// import config from 'package.json'
 
-const outputFile = 'lightbox.js'
+const config = require('./package.json')
+outputFile = config.name + '.js'
+console.log('Output File  = ' + outputFile)
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: outputFile,            // make it dynamic
+    filename: outputFile,
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -15,4 +18,10 @@ module.exports = {
     ]
   },
   devtool: 'eval',
+  plugins: [
+    new webpack.DefinePlugin({
+      // able to use this variable in my bundle files BUT NOT in webpack config itself
+      outputFile: JSON.stringify(require("./package.json").name),
+    })
+  ]
 };
